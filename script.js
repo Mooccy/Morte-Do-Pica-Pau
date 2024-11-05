574e8e74fc0a38a05a57ca022e681611b104a26f
Murilo de Lima Marezi <murilo.lima.marezi@hotmail.com>
2024-11-05 19:45:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function playFolha() {
+    let audio = document.getElementById("folha");
+    audio.play();
+}
+function playPica() {
+    let audio = document.getElementById("pica");
+    audio.play();
+}
+function playBase() {
+    let audio = document.getElementById("base");
+    audio.play();
+}
+function playHit() {
+    let audio = document.getElementById("hit");
+    audio.play();
+}
